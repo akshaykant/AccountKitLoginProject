b083b0f41cf136b0e9da84f0533b3c0a4a9cc598
akshaykant <akshaykant1708@gmail..com>
2017-04-26 17:29:25 +0530

    Account Kit with Mobile and Email

--- app/src/main/java/com/example/udacity/surfconnect/AccountActivity.java
+++ app/src/main/java/com/example/udacity/surfconnect/AccountActivity.java
@@ -3,7 +3,15 @@ package com.example.udacity.surfconnect;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.PhoneNumber;
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
@@ -26,6 +34,42 @@ public class AccountActivity extends AppCompatActivity {
         infoLabel = (TextView) findViewById(R.id.info_label);
         info = (TextView) findViewById(R.id.info);
 
+        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+            @Override
+            public void onSuccess(final Account account) {
+                // Get Account Kit ID
+                String accountKitId = account.getId();
+                id.setText(accountKitId);
+
+                PhoneNumber phoneNumber = account.getPhoneNumber();
+                if (account.getPhoneNumber() != null) {
+                    // if the phone number is available, display it
+                    String formattedPhoneNumber = formatPhoneNumber(phoneNumber.toString());
+                    info.setText(formattedPhoneNumber);
+                    infoLabel.setText(R.string.phone_label);
+                }
+                else {
+                    // if the email address is available, display it
+                    String emailString = account.getEmail();
+                    info.setText(emailString);
+                    infoLabel.setText(R.string.email_label);
+                }
+
+            }
+
+            @Override
+            public void onError(final AccountKitError error) {
+                // display error
+                String toastMessage = error.getErrorType().getMessage();
+                Toast.makeText(AccountActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void onLogout(View view) {
+        // logout of Account Kit
+        AccountKit.logOut();
+        launchLoginActivity();
     }
 
     private void launchLoginActivity() {
@@ -34,6 +78,7 @@ public class AccountActivity extends AppCompatActivity {
         finish();
     }
 
+    // library for parsing, formatting, and validating international phone numbers
     private String formatPhoneNumber(String phoneNumber) {
         // helper method to format the phone number for display
         try {
